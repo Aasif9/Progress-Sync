a3067b93375a3723dd8d29bcd446ff79b1f9f54f
Asif Ali <62379306+Aasif9@users.noreply.github.com>
2023-02-24 23:42:40 +0530

    Update index.java

--- src/student_result_management_system/index.java
+++ src/student_result_management_system/index.java
@@ -114,7 +114,6 @@ public class index extends javax.swing.JFrame {
         });
     }
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
